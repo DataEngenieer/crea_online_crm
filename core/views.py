cb9c22c46e875166e39d3cf90733aba58dbe2d61
Jhon Moreno <liderinnovacion@gsco.com.co>
2025-06-07 11:34:48 -0500

    Ajustes generales 2

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.urls import reverse
 from .utils import determinar_estado_cliente
 from django.core.paginator import Paginator
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
